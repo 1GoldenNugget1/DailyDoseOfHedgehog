e9a3a7ff34aa6ac3a999bf0e5b9e7cadbc35f023
1GoldenNugget1 <76992055+1GoldenNugget1@users.noreply.github.com>
2024-08-30 19:17:24 +0200

    added date and time https://www.npmjs.com/package/date-and-time

--- index.mjs
+++ index.mjs
@@ -1,7 +1,12 @@
 import { TwitterApi } from 'twitter-api-v2';
 import dotenv from 'dotenv';
 import fs from 'fs'
+import date from 'date-and-time';
 dotenv.config()
+
+//setting up new date 
+const now = new Date();
+date.format(now, 'ddd, MMM DD YYYY HH:mm:ss');
 
 //Create new twitter client with oauth 1.0
 const userClient = new TwitterApi({
@@ -21,5 +26,5 @@ let randomFile = "img/"+files[Math.floor(Math.random() * files.length)];
 //Upload image to Twitter and get its ID
 const mediaId = await userClient.v1.uploadMedia(randomFile);
 //Post a tweet with image's ID
-await userClient.v2.tweet("🦔", {media: {media_ids: [mediaId]}});
+await userClient.v2.tweet(`🦔 ${now}`, {media: {media_ids: [mediaId]}});
 
